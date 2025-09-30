6df119fc061cc63ecb17092857cf4b74f392d199
Lucas Coelho Duarte <lucascduarte99@gmail.com>
2025-09-29 21:36:32 -0300

    Implementa atualizações e melhorias

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,10 @@
 jQuery(($) => {
+    iniciaAcoesScroll();
+    iniciaMenuHeader();
+    iniciaCarrosselProjetos();
+});
+
+function iniciaAcoesScroll(){
     $(window).on('scroll', () => {
         if ($(this).scrollTop() >= 100) {
             $('#header-principal').addClass('scroll');
@@ -20,6 +26,69 @@ jQuery(($) => {
     });
 
     $('#button-return-top').click(() => {
-        $('html, body').animate({ scrollTop: 0 }, 150);
+        if (!$('html, body').is(":animated")) {
+            $('html, body').animate({ scrollTop: 0 }, 600, 'linear');
+        }
     });
-});
+}
+
+function iniciaMenuHeader(){
+    $('#header-principal #menu-header .button').click(() => {
+        if($('#header-principal').hasClass('open-menu')){
+            $('#header-principal').removeClass('open-menu');
+        } else {
+            $('#header-principal').addClass('open-menu');
+        }
+    });
+
+    $('#header-principal .menu-item label').click((event) => {
+        ativaMenu($(event.target).parent());
+
+        secaoDestino = "#" + $(event.target).parent().attr("data-id")
+        
+        $("html, body").stop().animate({
+            scrollTop: $(secaoDestino).offset().top - 150
+        }, 600, 'linear');
+    });
+
+    $(window).on("scroll", function(event) {
+
+        let window = $(this);
+
+        if(!$("html, body").is(':animated')){
+            $.each($('#header-principal .menu-item'), function(index, menu) {
+                dataIdMenu = $(menu).attr("data-id");
+
+                if(window.scrollTop() > ($('#' + dataIdMenu).offset().top - 150)
+                    && window.scrollTop() < ($('#' + dataIdMenu).offset().top - 150 + $("#" + dataIdMenu).height())){
+                    ativaMenu($("#header-principal .menu-item[data-id='" + dataIdMenu + "']"));
+                }
+            });
+        }
+    });
+}
+
+function ativaMenu(menu){
+    if (!$(menu).hasClass('ativo')) {
+        $('#header-principal .menu-item').removeClass('ativo');
+        menu.addClass('ativo');
+        
+        larguraAreaMenu = $("#header-principal .area-menu").width();
+        posicaoMenuAtivoPX = $("#header-principal .menu-item.ativo").offset().left - $("#header-principal .area-menu").offset().left;
+
+        posicaoMenuAtivoPorcentagem = (100 / larguraAreaMenu) * posicaoMenuAtivoPX + "%";
+        
+        $("#header-principal .indicador-selecao").css("left", posicaoMenuAtivoPorcentagem);
+    }
+}
+
+function iniciaCarrosselProjetos() {
+    $('#secao-projetos .grid .area-projetos .carrossel-projetos').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: $('#secao-projetos .grid .area-projetos .button-anterior'),
+        nextArrow: $('#secao-projetos .grid .area-projetos .button-proximo')
+    });
+}
